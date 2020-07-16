c9b0301c5fd422c1ed02a13e06737bce4e6474f6
nasicseiiit <nasicseiiit@gmail.com>
2020-07-16 10:24:11 +0530

    Creat react app and changed the port from 3000 to 8080

---
+++ react-todo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-todo/src/components/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contacts = () => {
+    return(
+        <div
+        className='ui raised very padded text cintainer segment'
+        style={{marginTop:'80px'}}
+        >
+        <h3 className='ui header'>Contacts</h3>
+        <p>Lorem ipsum dolor sit amet, consecutor</p>
+        </div>
+    )
+
+}
+
+export default Contacts;
